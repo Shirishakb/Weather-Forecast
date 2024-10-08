dd966986e90b214158cd018d5145fccccd184262
ShirishaKb <shirishakb@gmail.com>
2024-10-08 18:05:32 -0400

    modified

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the client dist folder
-app.use(express.static(join(__dirname, '../client/dist')));
+app.use(express.static(join(__dirname, '/../../client/dist')));
 
 // Connect the routes
 app.use(routes);
